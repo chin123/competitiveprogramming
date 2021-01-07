e613a7bb854d03e33b06bc22ef97993f44d32deb
Chinmaya Krishnan Mahesh <chinmaya.mahesh@disroot.org>
2021-01-07 00:25:37 -0600

    e done

--- codeforces/693_3/e.cpp
+++ codeforces/693_3/e.cpp
@@ -11,6 +11,8 @@ int main() {
 	int t;
 	cin >> t;
 	for (int c = 0; c < t; c++) {
+		ans.clear();
+		ind.clear();
 		int n;
 		cin >> n;
 		for (int i = 0; i < n; i++) {
@@ -26,37 +28,54 @@ int main() {
 		sort(war,war+n);
 		priority_queue<pair<unsigned long long, unsigned long long int>, vector<pair<unsigned long long, unsigned long long int>>, greater<pair<unsigned long long, unsigned long long int>> > ws;
 		priority_queue<pair<unsigned long long, unsigned long long int>, vector<pair<unsigned long long, unsigned long long int>>, greater<pair<unsigned long long, unsigned long long int>> > ws2;
-		vector<pair<unsigned long long, unsigned long long>> temp;
+		vector<pair<unsigned long long, unsigned long long>> prefs;
 		ws.push({ar[0].second, ar[0].first});
+		prefs.push_back(ws.top());
 		for (int i = 1; i < n; i++) {
-			if (temp.size() != 0 && ar[i].first > temp.back().first) {
-				for (auto m: temp) {
-					ws.push({m.second, m.first});
-				}
-				temp.clear();
+			ws.push({ar[i].second, ar[i].first});
+			prefs.push_back(ws.top());
+		}
+		for (int i = 0; i < n; i++) {
+			auto h = ar[i].first;
+			auto w = ar[i].second;
+			//cout << "looking " << h << "," << w << endl;
+			auto it = lower_bound(ar, ar+n, pair<unsigned long long, unsigned long long>(h, 0));
+			//cout << "found " << it->first << "," << it->second << endl;
+			if (it != ar) {
+				it--;
+				//cout << "subtract: " << it->first << "," << it->second << endl;
+				auto m = prefs[(it - ar)];
+				//cout << "pref: " << m.first << "," << m.second << endl;
+				if (m.second < ar[i].first && m.first < ar[i].second)
+					ans[ar[i]] = ind[{m.second, m.first}];
 			}
-			auto m = ws.top();
-			if (m.second < ar[i].first && m.first < ar[i].second) {
-				ans[ar[i]] = ind[{m.second, m.first}];
-			}
-			temp.push_back(ar[i]);
+			//cout << "first pass: " << ans[ar[i]] << endl;
 		}
 
-		temp.clear();
 
 		ws2.push({war[0].second, war[0].first});
+		prefs.clear();
+		prefs.push_back(ws2.top());
 		for (int i = 1; i < n; i++) {
-			if (temp.size() != 0 && war[i].first > temp.back().first) {
-				for (auto m: temp) {
-					ws2.push({m.second, m.first});
+			ws2.push({war[i].second, war[i].first});
+			prefs.push_back(ws2.top());
+		}
+		for (int i = 0; i < n; i++) {
+			auto w = ar[i].first;
+			auto h = ar[i].second;
+			//cout << "looking " << w << "," << h << endl;
+			auto it = lower_bound(war, war+n, pair<unsigned long long, unsigned long long>(w, 0));
+			//cout << "found " << w << "," << h << " " << it->first <<","<< it->second << endl;
+			if (it != war) {
+				it--;
+				//cout << "subtracted and reached " << it->first << ","<<it->second<<endl;
+				auto m = prefs[(it - war)];
+				//cout << "pref: " << m.first << "," << m.second << endl;
+				if (m.second < ar[i].first && m.first < ar[i].second) {
+					ans[ar[i]] = ind[m];
 				}
-				temp.clear();
+				//cout << "second pass: " << ans[ar[i]] << endl;
 			}
-			auto m = ws2.top();
-			if (m.second < war[i].second && m.first < war[i].first) {
-				ans[{war[i].second, war[i].first}] = ind[m];
-			}
-			temp.push_back(ar[i]);
 		}
 
 		for (int i = 0; i < n; i++) {
